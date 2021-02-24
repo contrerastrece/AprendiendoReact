fa0b5f59aba4296dbb8941764d3efed198ea3589
vcontreras <contrerastrece@gmail.com>
2021-02-23 22:57:56 -0500

    Componente creado con una Funcion declarada

--- src/components/Componente.js
+++ src/components/Componente.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
+// Componente creado con Clase
 
-class Componente extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Hello I'm a Componente</h2>
-        <p>{this.props.msg}</p>
+// class Componente extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>Hello I'm a Componente</h2>
+//         <p>{this.props.msg}</p>
 
-      </div>
-    )
-  }
+//       </div>
+//     )
+//   }
+// }
+
+// componente creado con Function declarada
+function Componente(props) {
+  return (
+    <div>
+      <h2>Hello I'm a Componente Funcional</h2>
+      <p>{props.msg}</p>
+
+    </div>
+  )
 }
+
+
 export default Componente;
